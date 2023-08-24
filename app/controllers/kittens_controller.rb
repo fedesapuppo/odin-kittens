f4d7ed7a76f42a1240066f2aff1c6909d040ee2e
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-08-24 14:15:49 +0300

    add flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -14,21 +14,21 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
+      flash[:success] = "Kitten added successfully!"
       redirect_to @kitten
     else
+      flash[:error] = "Error adding kitten. Please check the form."
       render "new"
     end
-  end
-
-  def edit
-    @kitten = Kitten.find(params[:id])
   end
 
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
+      flash[:success] = "Kitten updated successfully!"
       redirect_to @kitten
     else
+      flash[:error] = "Error updating kitten. Please check the form."
       render "edit"
     end
   end
@@ -36,7 +36,12 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
+    flash[:success] = "Kitten deleted successfully!"
     redirect_to kittens_path
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   private
